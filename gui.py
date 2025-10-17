32f653764400885078182d558a576a9856cf798d
mrnrz <mrnrznv@gmail.com>
2025-10-17 15:08:57 +0300

    implement add button

--- gui.py
+++ gui.py
@@ -1,23 +1,35 @@
 import FreeSimpleGUI as Sg
 import modules.functions as f
 
-todos = f.get_list_form_file()
-todos = [item.replace('\n','') for item in todos]
-print(todos)
+#todos = f.get_list_form_file()
+#todos_to_show = [item.replace('\n','') for item in todos]
 
 label = Sg.Text('Type a to-do')
-input_box = Sg.InputText(key='input_box', default_text="", tooltip="Enter to-do")
-input_list = Sg.Listbox(values=todos)
+input_box = Sg.InputText(key='todo', default_text="", tooltip="Enter to-do")
+#input_list = Sg.Listbox(values=todos_to_show)
 add_button = Sg.Button('Add', key='add_button', tooltip="Add new item")
 
-window = Sg.Window('My To-Do App', layout=[
+window = Sg.Window('My To-Do App',
+                   layout=[
     [label],
     [input_box],
-    [input_list],
+    #[input_list],
     [add_button]
-])
+],
+                   font=('Helvetica', 18))
 
-window.read()
-print('Hello')
+while True:
+    event, values = window.read()
+    print(event)
+    print(values['todo'])
+    match event:
+        case 'add_button':
+            todos = f.get_list_form_file()
+            new_todo = values['todo'] + '\n'
+            todos.append(new_todo)
+            f.update_file(todos)
+        case Sg.WIN_CLOSED:
+            break
+
 window.close()
 
